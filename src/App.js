6a198be73bc4b2bd7a17d5a392c613b7c8a77738
Сергей Шабала <13redfox@MacBook-Pro-Sergej.local>
2021-06-28 13:15:03 +0300

    add: project complete

--- src/App.js
+++ src/App.js
@@ -6,18 +6,22 @@ import Footer from './components/Footer'
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import Category from './pages/Category';
+import Recipe from './pages/Recipe';
 import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <>
-      <Router>
+      <Router basename="/react-food">
         <Header />
         <main className="container content">
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/about" component={About} />
             <Route path="/contact" component={Contact} />
+            <Route path="/category/:name" component={Category} />
+            <Route path="/meal/:id" component={Recipe} />
             <Route component={NotFound} />
           </Switch>
         </main>
